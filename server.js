883a897c339f11ebea6209de0da3bb75c0a50b06
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2024-07-17 19:08:14 +0530

    updated socket

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://post-it-jt47.onrender.com/","https://nullhawk.github.io/post-it/", "https://nullhawk.github.io/"], //to change with hosting url
+    origin: ["http://localhost:3000", "https://post-it-jt47.onrender.com/","https://nullhawk.github.io/post-it/", "https://nullhawk.github.io"], //to change with hosting url
   },
 });
 
